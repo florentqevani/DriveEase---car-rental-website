f79e675ec7dd3e43a629400bad54935df1ccdff7
Admin <flori030201@gmail.com>
2026-03-31 10:06:17 +0200

    .

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,4 +1,4 @@
-const { hash: _hash, compare } = require('bcrypt');
+const { hash: _hash, compare } = require('bcryptjs');
 const { OAuth2Client } = require('google-auth-library');
 const { query } = require('../config/db');
 const { signAccessToken, generateRefreshToken, saveRefreshToken, revokeRefreshToken, findRefreshToken } = require('../middleware/auth');
